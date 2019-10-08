def2db89735dcbe9727405a3d594626baa46fd9e
ferdinandmwal123 <ferdinandmwal@gmail.com>
2019-10-08 11:18:20 +0300

    Toast displays success

---
+++ app/src/main/java/com/mwalagho/ferdinand/cosocial/CompaniesActivity.java
@@ -0,0 +1,6 @@
+package com.mwalagho.ferdinand.cosocial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CompaniesActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/mwalagho/ferdinand/cosocial/MainActivity.java
+++ app/src/main/java/com/mwalagho/ferdinand/cosocial/MainActivity.java
@@ -3,12 +3,37 @@ package com.mwalagho.ferdinand.cosocial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button3) Button mRegister;
+    @BindView(R.id.cList) ListView mListView;
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Registered Successfully!",Toast.LENGTH_LONG).show();
+                Log.i(TAG,"User saved");
+            }
+        });
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_activated_1,companies);
+        mListView.setAdapter(adapter);
+
     }
 }
