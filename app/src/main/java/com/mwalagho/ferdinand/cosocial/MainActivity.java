4c919fcc924525cdf705909986fb3ba73c3220f0
ferdinandmwal123 <ferdinandmwal@gmail.com>
2019-10-09 12:51:06 +0300

    Intent works!

--- app/src/main/java/com/mwalagho/ferdinand/cosocial/MainActivity.java
+++ app/src/main/java/com/mwalagho/ferdinand/cosocial/MainActivity.java
@@ -5,22 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
-
+import android.view.GestureDetector;
 import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener{
 
     @BindView(R.id.button3) Button mRegister;
-//    @BindView(R.id.cList) ListView mListView
+    public GestureDetector gestureDetector;
     public static final String TAG = MainActivity.class.getSimpleName();
     ArrayList<String> names = new ArrayList<>();
     @BindView(R.id.cName) EditText name;
@@ -30,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        this.gestureDetector = new GestureDetector(this,this);
+        gestureDetector.setOnDoubleTapListener(this);
 
         mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,10 +43,60 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this,CompaniesActivity.class);
                 intent.putExtra("names",names);
                 startActivity(intent);
+
+
                 Toast.makeText(MainActivity.this,"Registered Successfully!",Toast.LENGTH_LONG).show();
+                System.out.println(names);
                 Log.i(TAG,"User saved");
             }
         });
 
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        Toast.makeText(MainActivity.this,"Welcome! \uD83D\uDE0A",Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        Toast.makeText(MainActivity.this,"We need your details \uD83D\uDE0A",Toast.LENGTH_LONG).show();
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
     }
 }
